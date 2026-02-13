1d58e728ecedb81dd24e72a34c78fc22a927822d
Mohamed BOUZAYANI <147999857+MohamedBouzayani@users.noreply.github.com>
2026-02-13 17:07:42 +0100

    15

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,5 +1,6 @@
+import os
 from ultralytics import YOLO
 
-model = YOLO('../outputs/model_weights/model_a_best.pt')
-metrics = model.val(data='../data/coco128.yaml')
+model = YOLO('/home/runner/work/MLOps/MLOps/outputs/model_weights/model_a_best.pt')
+metrics = model.val(data=os.path.abspath('/home/runner/work/MLOps/MLOps/data/coco128.yaml'))
 print(metrics)
