b9bfa5c70117e3d37235855829909156f7783927
Mohamed BOUZAYANI <147999857+MohamedBouzayani@users.noreply.github.com>
2026-02-21 14:58:53 +0100

    16

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,6 +1,16 @@
+#import os
+#from ultralytics import YOLO
+
+#model = YOLO('/home/runner/work/MLOps/MLOps/outputs/model_weights/model_a_best.pt')
+#metrics = model.val(data=os.path.abspath('/home/runner/work/MLOps/MLOps/data/coco128.yaml'))
+#print(metrics)
 import os
 from ultralytics import YOLO
 
-model = YOLO('/home/runner/work/MLOps/MLOps/outputs/model_weights/model_a_best.pt')
+model_path = '/home/runner/work/MLOps/MLOps/outputs/model_weights/model_a_best.pt'
+if not os.path.exists(model_path):
+    raise FileNotFoundError(f"{model_path} not found! Did training and copy step complete successfully?")
+
+model = YOLO(model_path)
 metrics = model.val(data=os.path.abspath('/home/runner/work/MLOps/MLOps/data/coco128.yaml'))
 print(metrics)
