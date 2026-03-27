973637895aac9002101424bc7da6cc3f8dd2b602
Mohamed BOUZAYANI <147999857+MohamedBouzayani@users.noreply.github.com>
2026-03-27 16:34:03 +0100

    fixed eval

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -6,7 +6,7 @@ model_path = (
 )
 if not os.path.exists(model_path):
     raise FileNotFoundError(
-        f"{model_path} not found! Did training and copy step complete successfully?"
+        f"{model_path} not found! Did training and copy step complete success?"
     )
 
 model = YOLO(model_path)
